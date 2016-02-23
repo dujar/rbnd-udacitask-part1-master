77658b3b68bcf18071ef5e444f21d2e19f5f5c55
dujar <dujar.coding@gmail.com>
2016-02-23 09:09:13 +0100

    list + items + change name of list

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,77 @@
 class TodoList
     # methods and stuff go here
+    attr_accessor :title, :items
+    def title
+    @title
+    end
+
+    def items
+    @items
+    end
+
+    def initialize(list_title)
+        @title = list_title
+        @items = Array.new
+    end
+
+    def add_item(new_item, new_frequency, options = {})
+        item = Item.new new_item, new_frequency
+        @items.push item
+    end
+
+    def print_list
+        puts "~~~~~~~~~~~~~~~~~~~~~~~"
+        puts "Title list: #{@title}"
+        puts "~~~~~~~~~~~~~~~~~~~~~~~"
+        print_items
+    end
+
+    def print_items
+        @items.each do |item|
+            item.print_each_item
+        end
+    end
+
+    def remove_by_id(item)
+        l = item + 1
+        s = item - 1
+        items = @items[s..l]
+        @items-items
+    end
+
+    def completed_status completed
+        find 
+        @Complete_status = completed
+    end
+
+    def show_items
+        puts @items
+    end
+
 end
 
 class Item
     # methods and stuff go here
+    attr_accessor :description, :completed_status
+
+    @@number_of_id = 0
+    def initialize item_description, item_frequency, options = {}
+        
+        @ID = id_number
+        @description = item_description
+        @completed_status = false
+        @frequency = item_frequency
+        @due_date = options[:due]|| "within this month"
+        
+    end
+
+    def id_number
+        @@number_of_id += 1
+    end
+    def print_each_item
+        puts " #{@ID} #{@description} \t| #{@frequency} \t|#{@due_date} \t| Completed: #{@completed_status}"
+        #puts " #{@ID} #{@description} \t| #{@frequency}  \t| Completed: #{@completed_status}"
+    end
+
+
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,23 +1,35 @@
 require_relative 'todolist.rb'
+#require 'table_print'
 
 # Creates a new todo list
+list1 = TodoList.new("Ruby studies")
 
 # Add four new items
 
+list1.add_item("Check forums for help","all day", {due: "11/04/2016"})
+list1.add_item("read books about it","from time to time", {due: "11/04/2016"})
+list1.add_item("check online courses","regularly", {due: "11/04/2016"})
+list1.add_item("keep updated","hourly")
 # Print the list
-
+list1.print_list
 # Delete the first item
+list1
 
 # Print the list
+list1.print_list
 
 # Delete the second item
-
+list1.remove_by_id 1
 # Print the list
-
+list1.print_list
 # Update the completion status of the first item to complete
-
+#list1.items.completed_status = true
 # Print the list
+list1.print_list
 
 # Update the title of the list
-
+list1.title = "My new List"
 # Print the list
+list1.print_list
+
+list1.show_items.find(1)
